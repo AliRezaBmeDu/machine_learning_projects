0e3a94d652acd4916796307c1e08e6435cc6d217
AliRezaBmeDu <ali.reza.buet@gmail.com>
2024-04-08 02:57:54 +0600

    Load data

---
+++ spam_email_detection/spam_email_classifier.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 12:20:28 2024
+
+@author: Reza
+"""
+
+# Importing necessary libraries for EDA
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import string
+import nltk
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+nltk.download('stopwords')
+
+# Importing libraries necessary for Model Building and Training
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('../datasets/spam_ham_dataset.csv')
+data.head()
