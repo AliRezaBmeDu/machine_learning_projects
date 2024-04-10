9a141cf202ef477b65898d7fd1eb7506095b8deb
AliRezaBmeDu <ali.reza.buet@gmail.com>
2024-04-11 01:17:46 +0600

    Evaluate the model. Need to fix

--- classify-handwritten-digits/classify_handwritten_digits_using_tensorflow.py
+++ classify-handwritten-digits/classify_handwritten_digits_using_tensorflow.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+tf.config.run_functions_eagerly(True)
 # Load MNIST dataset
 mnist = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -17,28 +18,36 @@ mnist = tf.keras.datasets.mnist
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-# Display the first image from the training dataset
-plt.figure()
-plt.imshow(train_images[0], cmap='gray')
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# Flatten images
+train_images = train_images.reshape(-1, 28 * 28)
+test_images = test_images.reshape(-1, 28 * 28)
 
+# Define a function for preprocessing
+def preprocess(image, label):
+    return image / 255.0, label
 
+# Create TensorFlow dataset
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
 
-# Assign data and labels
-data = train_images
-labels = train_labels
-test_data = test_images
-test_labels = test_labels
+# Apply preprocessing using map function
+train_dataset, train_labels = train_dataset.map(preprocess)
+test_dataset. test_labels = test_dataset.map(preprocess)
 
-def display(i):
-	img = test_data[i]
-	plt.title('label : {}'.format(test_labels[i]))
-	plt.imshow(img.reshape((28, 28)))
-	
-# image in TensorFlow is 28 by 28 px
-display(0)
+# Define a simple linear model using Keras
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28 * 28,)),  # Flatten the input images
+    tf.keras.layers.Dense(10, activation='softmax')  # Output layer with 10 units and softmax activation
+])
 
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+# Train the model
+model.fit(train_dataset, train_labels, epochs=5, batch_size=100)
 
+# Evaluate the model on test data
+test_loss, test_accuracy = model.evaluate(test_dataset, test_labels)
+print('Test accuracy:', test_accuracy)
